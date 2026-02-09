2f8ad4bcf8bc2713ace4bdf6ca9a9b25578e9e71
souzabeatriz3005-alt <souzabeatriz3005@gmail.com>
2026-02-09 10:16:30 -0300

    Update laco1.js

--- laco1.js
+++ laco1.js
@@ -1,5 +1,5 @@
 // Iteração simples de 0 a 4
-for (let i = 0; i < 5; i++) {
+for (let i = 6; i < 12; i++) {
     // i++ é o incremento que evita o loop infinito
     console.log(`Contagem: ${i}`);
-}
+}   console.log(`acabou!
